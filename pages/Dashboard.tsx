563c4efebae4db21b87d1ac54857265dd7fd2435
tailedflox9-maker <tailedflox9@gmail.com>
2025-11-16 23:35:08 +0530

    Update Dashboard.tsx

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { getAnalyticsSummary, getRecentVisits, fetchBusinesses } from '../supabaseClient';
 import { AnalyticsSummary, VisitLog, Business } from '../types';
